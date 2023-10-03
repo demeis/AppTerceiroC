c5ec2e13a6409aebee058595b73cc644c7b9f326
LUCAS GOMES MOREIRA <RM77943@fiap.com.br>
2023-10-03 11:56:31 -0300

    Aula 01

---
+++ AppTerceiroC/AppTerceiroC/Conexao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroC
+{
+    public static class Conexao
+    {
+        public static string strConexao = "server=bancomodulo.ddns.net;port=3306;user=seu.usuario;password=sua.senha;database=seu.banco;";
+    }
+}
